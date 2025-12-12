9e234c893afc19515c741b6a7f7d27a6cdaf8001
tommy y <tommy.yohanes@tuntun.co.id>
2025-12-12 12:23:56 +0700

    init

---
+++ storyline/storylineApp.swift
@@ -0,0 +1,17 @@
+//
+//  storylineApp.swift
+//  storyline
+//
+//  Created by tommy yohanes on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct storylineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ storylineTests/storylineTests.swift
@@ -0,0 +1,16 @@
+//
+//  storylineTests.swift
+//  storylineTests
+//
+//  Created by tommy yohanes on 12/12/25.
+//
+
+import Testing
+
+struct storylineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
